50dc6393ca104e96b97a985d250d99c19f34f13a
Giao U <lesliegiao.u@gmail.com>
2023-08-15 16:49:35 -0700

    Added data.dart to store classes for Task and TaskList

---
+++ lib/data.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+
+class Task {
+  String? title;
+  String? description;
+  String? status;
+
+  Task( this.title, this.description, this.status);
+}
+
+class TaskList with ChangeNotifier{
+  final List<Task> _taskList = [Task('Add Task feature','Make the flutter app be able to list the added tasks','in progress'),
+                       Task('Watch Youtube','Watch episode 2','open')];
+
+
+  int _currentIndex = 0;
+
+  List<Task> get taskList => _taskList.toList();
+  firstTask() => _taskList[_currentIndex];
+
+  addTask(Task task){
+    _taskList.add(task);
+    notifyListeners();
+  }
+
+  updateCurrentTask(int index){
+    _currentIndex = index;
+    notifyListeners();
+  }
+
+
+
+}
