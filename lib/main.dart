ab073ac059852ae3fa15f0cd4ef13210d9ae1436
Giao U <lesliegiao.u@gmail.com>
2023-08-15 16:48:58 -0700

    Added a Listview to display all the titles of tasks and added changenotifierprovider to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:task_tracker/data.dart';
 import 'package:task_tracker/task_form.dart';
 
 void main() {
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    /*return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -20,6 +22,18 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(title: 'Task Tracker_GiaoU'),
       //home:const TaskForm(),
+    );*/
+    return ChangeNotifierProvider(
+      create: (context)=>TaskList(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'Task Tracker_GiaoU'),
+      ),
     );
   }
 }
@@ -36,22 +50,37 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final taskHolder = context.watch<TaskList>();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body:  ListView.builder(
+            /*shrinkWrap: true,
+            itemCount: taskHolder.taskList.length,
+            itemBuilder: ((context, index) => Card(
+                margin: const EdgeInsets.all(8.0),
+                child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(taskHolder.taskList[index].title!)
+                )
+            )),*/
+        itemCount: taskHolder.taskList.length,
+        itemBuilder: (context,index){
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundColor: const Color(0xff764abc),
+              child: Text(index.toString()),
             ),
-          ],
-        ),
+            title: Text(taskHolder.taskList[index].title!),
+            subtitle: Text(taskHolder.taskList[index].description!),
+            trailing: const Icon(Icons.more_vert),
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const TaskForm()));
+            },
+          );
+        }
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
