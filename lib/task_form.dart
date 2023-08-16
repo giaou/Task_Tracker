f65a8ce95a416fdb797e8ca92e91a528fc8817b8
Giao U <lesliegiao.u@gmail.com>
2023-08-15 22:03:41 -0700

    Modified text controller to set initial value in Edit Mode

--- lib/task_form.dart
+++ lib/task_form.dart
@@ -4,7 +4,13 @@ import 'package:task_tracker/data.dart';
 //List of tasks' statuses
 List<String> list = <String>['Open','In Progress','Complete'];
 class TaskForm extends StatefulWidget {
-  const TaskForm({super.key});
+  TaskForm({this.title, this.description, this.status, super.key});
+  String? title;
+  String? description;
+  String? status;
+
+
+
 
 
   @override
@@ -15,21 +21,32 @@ class _TaskFormState extends State<TaskForm> {
 
   //define the global key
   final _formKey = GlobalKey<FormState>();
+
+
   //Text Controllers
 
-  TextEditingController title = TextEditingController();
-  TextEditingController description = TextEditingController();
-  TextEditingController status = TextEditingController();
-
-
-
+  TextEditingController titleHandler = TextEditingController();
+  TextEditingController descriptionHandler = TextEditingController();
   String dropdownValue = list.first;
+
+  @override
+  void initState(){
+    super.initState();
+    if(widget.title!=null && widget.description!=null &&widget.status!=null){
+      titleHandler.text = widget.title!;
+      descriptionHandler.text = widget.description!;
+      dropdownValue = widget.status!;
+    }
+  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Create Task'),
+        title: widget.title!=null? const Text('Edit Task'):const Text('Create Task'),
       ),
       body: Form(
         key: _formKey,
@@ -37,7 +54,10 @@ class _TaskFormState extends State<TaskForm> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextFormField(
-              controller: title,
+              controller: titleHandler,
+              decoration: const InputDecoration(
+                  labelText: 'Title'
+              ),
               validator: (value){
                 if(value==null||value.isEmpty){
                   return 'Title of the task cannot be empty';
@@ -46,7 +66,10 @@ class _TaskFormState extends State<TaskForm> {
               },
             ),
             TextFormField(
-              controller: description,
+              controller: descriptionHandler,
+              decoration: const InputDecoration(
+                labelText: 'Description'
+              ),
               validator: (value){
                 if(value==null||value.isEmpty){
                   return 'Description of the task cannot be empty';
@@ -72,8 +95,8 @@ class _TaskFormState extends State<TaskForm> {
             ElevatedButton(
                 onPressed: (){
                   if(_formKey.currentState!.validate()){
-                    context.read<TaskList>().addTask(Task(title.text,description.text,dropdownValue));
-                    print('Title: ${title.text}, description: ${description.text}, status: $dropdownValue');
+                    context.read<TaskList>().addTask(Task(titleHandler.text,descriptionHandler.text,dropdownValue));
+                    print('Title: ${titleHandler.text}, description: ${descriptionHandler.text}, status: $dropdownValue');
                   }
                 },
                 child:  Text('Submit'),
