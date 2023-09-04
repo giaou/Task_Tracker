9638e2f6e3315925ccbc7e7abc62a8acf58401dd
Giao U <lesliegiao.u@gmail.com>
2023-09-03 21:31:09 -0700

    Added relationship class to data.dart

--- lib/data.dart
+++ lib/data.dart
@@ -1,19 +1,26 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Task {
+  int id;
   String? title;
   String? description;
   String? status;
   String? updateTime;
+  List<Relationship> relationships;
 
-  Task( this.title, this.description, this.status, this.updateTime);
+  Task(this.id, this.title, this.description, this.status, this.updateTime, this.relationships);
+}
+
+class Relationship{
+  int? relatedTaskID;
+  String? label; //relationship between 2 tasks
+  Relationship(this.relatedTaskID,this.label);
 }
 
 class TaskList with ChangeNotifier{
-  final List<Task> _taskList = [Task('Add Task feature','Make the flutter app be able to list the added tasks','Complete',"unknown"),
-                       Task('Watch Youtube','Watch episode 2','Open',"unknown"),
-                       Task('Add the filter','Users will be able to show all tasks or tasks with a specific status','In Progress','unknown')];
-
+  final List<Task> _taskList = [Task(1,'Add Task feature','Make the flutter app be able to list the added tasks','Complete',"unknown",[Relationship(2,'is blocked by')]),
+                       Task(2,'Watch Youtube','Watch episode 2','Open',"unknown",[Relationship(3,'is subtask of')]),
+                       Task(3,'Add the filter','Users will be able to show all tasks or tasks with a specific status','In Progress','unknown',[Relationship(1,'is alternative to')])];
 
   int _currentIndex = 0;
   // get the whole list
@@ -32,6 +39,11 @@ class TaskList with ChangeNotifier{
       _taskList[oldIndex] = newTask;
     }
     notifyListeners();
+  }
+
+  getTaskFromID(int? relatedTaskID){
+    Task? relatedTask = _taskList.firstWhere((task) => task.id==relatedTaskID);
+    return relatedTask?.title??"unknown";
   }
 
 }
